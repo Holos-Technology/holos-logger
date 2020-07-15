497dc169b4608874aafb4425a0c20a8ff4ba9791
Luis Soria <lsoria@hasar.cl>
2020-07-15 19:00:37 -0300

    Inital commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const log4js=require("log4js");
+
+exports.InitLogger = function(app, filename, dirname, config) {
+  const pathLength = filename.replace(dirname + "/", "").split("/").length;
+  const serviceName = dirname.split("/").pop();
+  log4js.configure({
+    appenders: {
+      out: {
+        type: "stdout",
+        layout: {
+          type: "pattern",
+          pattern:
+            "%[%d %p %c %X{Pid} (%X{SessionTrace}) %f{" +
+            pathLength +
+            "}:%l:%o %m%]",
+        },
+      },
+    },
+    categories: {
+      default: {
+        appenders: ["out"],
+        level: config.logLevel,
+        enableCallStack: true,
+      },
+    },
+  });
+  const logger = log4js.getLogger(serviceName);
+  logger.addContext("Pid", process.pid);
+  console.log = logger.info.bind(logger);
+  console.error = logger.error.bind(logger);
+  console.warn = logger.warn.bind(logger);
+
+  // First middleware
+  app.use(function (req, res, next) {
+    logger.addContext("SessionTrace", req.headers["x-session-trace"]);
+    // Morgan replacement to set logger unified
+    if (req.method !== "OPTIONS")
+      console.log(
+        "Request ",
+        req.method,
+        " ",
+        req.url,
+        " from ",
+        req.headers["referer"]
+      );
+    return next();
+  });
+};
